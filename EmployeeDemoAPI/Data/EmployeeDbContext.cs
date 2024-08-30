740fcbb3c4cf4d05411e506031adbd7bb4a3085e
AHMAD AL AAMA <ahmadalaama@hotmail.com>
2024-08-30 21:49:52 +0300

    Create EF DB

---
+++ EmployeeDemoAPI/Data/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeDemoAPI.Model.Personnel;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDemoAPI.Data;
+
+public class EmployeeDbContext:DbContext
+{
+    public EmployeeDbContext(DbContextOptions contextOptions):base(contextOptions)
+    {
+            
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
---
+++ EmployeeDemoAPI/Model/Personnel/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeDemoAPI.Model.Personnel
+{
+    [Table("Personnel.Employee")]
+    public class Employee
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long EmployeeID { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public DateTime Birthdate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
