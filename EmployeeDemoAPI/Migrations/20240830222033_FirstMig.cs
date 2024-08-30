f1a4b0b0d3d75282d65bdf794f6d0ec959f83bda
AHMAD AL AAMA <ahmadalaama@hotmail.com>
2024-08-31 01:49:43 +0300

    test

---
+++ EmployeeDemoAPI/Migrations/20240830222033_FirstMig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeDemoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personnel.Employee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personnel.Employee", x => x.EmployeeID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personnel.Employee");
+        }
+    }
+}
