ea70a229182cdff161ffbe0c2623771e4fc1589c
AHMAD AL AAMA <ahmadalaama@hotmail.com>
2024-08-30 22:01:46 +0300

    test

--- EmployeeDemoAPI/Program.cs
+++ EmployeeDemoAPI/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Connection String
 builder.Services.AddDbContext<EmployeeDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDB")));
 
 var app = builder.Build();
